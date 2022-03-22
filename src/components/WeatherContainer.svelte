afbf776db358bf80fea6a1e01a0d0260f169d2ee
Zenpanic <sebastien.belmo@protonmail.com>
2022-03-22 14:55:41 +0100

    202203221455

--- src/components/WeatherContainer.svelte
+++ src/components/WeatherContainer.svelte
@@ -27,7 +27,7 @@
 	<WeatherData weather={forecastWeatherArray[time].weather[0]} main={forecastWeatherArray[time].main} forecastTime={forecastTime} wind={forecastWeatherArray[time].wind.speed} clouds={forecastWeatherArray[time].clouds.all} />
 	<nav class="navpage mt-2">
 		<button class="button is-primary is-light m-3" on:click={() => changeTime('down')} disabled={time <= 0}>Previous</button>
-  		<button class="button is-primary is-light m-3" on:click={() => changeTime('up')} disabled={time < forecastWeatherArray.length - 1 }>Next page</button>
+  		<button class="button is-primary is-light m-3" on:click={() => changeTime('up')} disabled={time >= forecastWeatherArray.length - 1 }>Next page</button>
 	</nav>
 </section>
 
